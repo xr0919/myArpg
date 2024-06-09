c406f42f2ed74d1d1a9d74114de95127bca084bc
Rong Xue <rn356837@dal.ca>
2024-06-09 13:07:24 -0300

    init repo

---
+++ Assets/Scripts/AnimationEvrnt/CharacterWeaponAnimationEvent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterWeaponAnimationEvent : MonoBehaviour
+{
+    [SerializeField] private Transform hipGS;
+    [SerializeField] private Transform handGS;
+    [SerializeField] private Transform handKatana;
+
+    private void ShowGS()
+    {
+        if (!handGS.gameObject.activeSelf)
+        {
+            handGS.gameObject.SetActive(true);
+            hipGS.gameObject.SetActive(false);
+            handKatana.gameObject.SetActive(false);
+        }
+    }
+
+    private void HideGS()
+    {
+        if (handGS.gameObject.activeSelf)
+        {
+            handGS.gameObject.SetActive(false);
+            hipGS.gameObject.SetActive(true);
+            handKatana.gameObject.SetActive(true);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Enemy/Health/AIHealthSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+namespace UGG.Health
+{
+    public class AIHealthSystem : CharacterHealthSystemBase
+    {
+
+
+
+        private void LateUpdate()
+        {
+            OnHitLockTarget();
+        }
+
+        public override void TakeDamager(float damagar, string hitAnimationName, Transform attacker)
+        {
+            SetAttacker(attacker);
+            _animator.Play(hitAnimationName, 0, 0f);
+            GameAssets.Instance.PlaySoundEffect(_audioSource, SoundAssetsType.hit);
+        }
+
+        private void OnHitLockTarget()
+        {
+            if (_animator.CheckAnimationTag("Hit"))
+            {
+                transform.rotation = transform.LockOnTarget(currentAttacker, transform, 50);
+            }
+        }
+    }
+
+}
